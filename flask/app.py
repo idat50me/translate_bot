0bc4e882cf61b7d1799ea542dd1278add8b37fcd
idat50me <59227194+idat50me@users.noreply.github.com>
2023-05-15 14:20:04 +0900

    nginxで動作するよう変更

--- flask/app.py
+++ flask/app.py
@@ -5,7 +5,6 @@ from slack_bolt import App
 from slack_bolt.adapter.flask import SlackRequestHandler
 from slack_sdk import WebClient
 from slack_bolt.adapter.socket_mode import SocketModeHandler
-from slack_bolt.adapter.flask import SlackRequestHandler
 from dotenv import load_dotenv
 
 from translate import jp_en_translate, en_jp_translate
@@ -15,20 +14,29 @@ from slack_sdk.errors import SlackApiError
 # .envファイルの内容を読み込見込む
 load_dotenv()
 
-logging.basicConfig(level=logging.INFO)
+# logging.basicConfig(level=logging.INFO, filename="logging.log")
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+fmt = logging.Formatter("%(asctime)s %(message)s")
+log_handler = logging.FileHandler("app.log")
+log_handler.setFormatter(fmt)
+logger.addHandler(log_handler)
 
-app = App()
-
+# app = App()
+app = App(
+    token=os.environ.get("SLACK_BOT_TOKEN"),
+    signing_secret=os.environ.get("SIGNING_SECRET") # signing_secret を追加しないと認証が通らず401になる
+)
 flask_app = Flask(__name__)
-handler = SlackRequestHandler(app)
 client = WebClient(os.environ["SLACK_BOT_TOKEN"])
-
-# app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 
 
 @app.message("hello")
 def message_hello(message, say):
-    say(
+    logger.info(f"hello message: {message['user']}")
+    say(f"Hey there <@{message['user']}>!")
+
+    """say(
         blocks=[
             {
                 "type": "section",
@@ -42,6 +50,7 @@ def message_hello(message, say):
         ],
         text=f"Hey there <@{message['user']}>!"
     )
+    """
     
 @app.event("reaction_added")
 def event_reactions_get(event, say):
@@ -68,6 +77,8 @@ def event_reactions_get(event, say):
         translate_text = messages[0]['text']
         reactions = messages[0]['reactions']
 
+        logging.info(f'messages: {messages}, reactions: {reactions}')
+
         start_en_postmessage = False
         start_jp_postmessage = False
 
@@ -87,6 +98,8 @@ def event_reactions_get(event, say):
         elif start_en_postmessage:
             text = str(en_jp_translate(translate_text))
 
+        logging.info(f'translated_text: {text}')
+
         attachments = json.dumps([
             {
                 "pretext": text,
@@ -100,13 +113,23 @@ def event_reactions_get(event, say):
     except SlackApiError as e:
         assert e.response["ok"] is False
         assert e.response["error"]  # str like 'invalid_auth', 'channel_not_found'
-        print(f"Got an error: {e.response['error']}")
+        logger.error(f"Got an error: {e.response['error']}")
+
+handler = SlackRequestHandler(app)
+
+@flask_app.route("/")
+def index():
+    return "Hello flask!"
 
 @flask_app.route("/slack/events", methods=["POST"])
 def slack_events():
+    p = request.json
+    logging.debug(p)
+    if p["token"] == os.environ["VERIFICATION_TOKEN"] and p["type"] == "url_verification": # verify用
+        return p["challenge"]
     return handler.handle(request)
 
 if __name__ == "__main__":
-    # SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
-    logging.basicConfig(format="%(asctime)s %(message)s", level=logging.DEBUG)
-    flask_app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 3000)))
+    # SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start() # ソケットモードによる通信
+    # logging.basicConfig(format="%(asctime)s %(message)s", level=logging.DEBUG)
+    flask_app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8081)))
--- flask/translate.py
+++ flask/translate.py
@@ -1,23 +1,30 @@
-import os
-from dotenv import load_dotenv
-import deepl
+# import os
+# from dotenv import load_dotenv
+# import deepl
+from translate_shell.translate import translate
 
 # .envファイルの内容を読み込見込む
-load_dotenv()
+# load_dotenv()
 
-API_KEY = os.environ['DEEPL_API_KEY']
-translator = deepl.Translator(API_KEY)
+# API_KEY = os.environ['DEEPL_API_KEY']
+# translator = deepl.Translator(API_KEY)
 
 def jp_en_translate(text):
-    source_lang = 'JA'
-    target_lang = 'EN-US'
-    result = translator.translate_text(text, source_lang=source_lang, target_lang=target_lang)
+    # source_lang = 'JA'
+    # target_lang = 'EN-US'
+    source_lang = "ja_JP"
+    target_lang = "en_US"
+    # result = translator.translate_text(text, source_lang=source_lang, target_lang=target_lang)
+    result = translate(text, target_lang=target_lang, source_lang=source_lang).results[0]["paraphrase"]
     return result
     
 def en_jp_translate(text):
-    source_lang = 'EN'
-    target_lang = 'JA'
-    result = translator.translate_text(text, source_lang=source_lang, target_lang=target_lang)
+    # source_lang = 'EN'
+    # target_lang = 'JA'
+    source_lang = "en_US"
+    target_lang = "ja_JP"
+    # result = translator.translate_text(text, source_lang=source_lang, target_lang=target_lang)
+    result = translate(text, source_lang=source_lang, target_lang=target_lang).results[0]["paraphrase"]
     return result
 
 if __name__ == '__main__':
